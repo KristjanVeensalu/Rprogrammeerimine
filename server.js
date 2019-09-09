c39ac1310b76b931dc37f8f44a208c4238b61f1a
Guren <Kristjan.Veensalu@gmail.com>
2019-09-09 14:33:27 +0300

    port

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 
 app.use(express.static('epood'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
