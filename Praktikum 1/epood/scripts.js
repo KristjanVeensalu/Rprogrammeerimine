1b93d6ac2dcd23fe15859c9f6519e94db737eaca
Guren <Kristjan.Veensalu@gmail.com>
2019-09-09 13:25:06 +0300

    mems

---
+++ Praktikum 1/epood/scripts.js
@@ -0,0 +1,26 @@
+    const x = window.location;
+    console.log(x);
+
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const title = urlParams.get("title");
+    const cost = urlParams.get("cost");
+    const src = urlParams.get("src");
+    const combined = "Title: " + title;
+    const combined2 = " Cons: " +cost;
+    const combined3 = " Path: " + src;
+    console.log();
+    alert(combined + combined2+ combined3)
+
+
+
+
+
+    const element = document.createElement("div");
+    element.append(document.createTextNode("Hello World! Testing"))
+
+
+    window.addEventListener("load")
+    const app = document.getElementById("item-body");
+
+    app.append(element);
