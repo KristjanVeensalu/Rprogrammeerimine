93e8854bccbd3309b3cb0cb5db5729840ce3df27
Guren <Kristjan.Veensalu@gmail.com>
2019-09-12 17:02:11 +0300

    Lesson2: CreateElement

--- epood/item.js
+++ epood/item.js
@@ -11,8 +11,32 @@
     alert(combined + combined2+ combined3)
 
 
-    const element = document.createElement("div");
-    element.append(document.createTextNode("Hello World!"));
+    const container = document.createElement("div");
+    container.className = "itemContainer";
 
-    const app = document.getElementById("item-body");
-    app.append(element);
+    const image = document.createElement("img");
+    image.src = src;
+    image.className = "item_image";
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = title;
+    titleElement.className = "item_title";
+
+    const description = "sisutekst";
+    const textElement = document.createElement("p");
+    textElement.textContent = description;
+    textElement.className = "item_description";
+
+    const costElement = document.createElement("div");
+    costElement.textContent = cost;
+    costElement.className = "item_price";
+
+    container.append(image);
+    container.append(titleElement);
+    container.append(textElement);
+    container.append(costElement);
+
+      window.addEventListener("load", () =>{
+        const app = document.getElementById("item-body");
+        app.append(container);
+    });
