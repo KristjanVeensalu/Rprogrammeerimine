18264bf2ab0759fb857c7ae74f7baa847141c9ea
Guren <Kristjan.Veensalu@gmail.com>
2019-09-12 20:13:05 +0300

    require testrun (commented out) header is now sticky, 4 products per row

---
+++ node_demo/main.js
@@ -0,0 +1,5 @@
+const utils = require("./utils");
+
+
+console.log("main.js loaded");
+console.log(utils.addTwoNumbers(2,4));
---
+++ node_demo/utils.js
@@ -0,0 +1,6 @@
+console.log("utils.js loaded");
+const addTwoNumbers = (a,b) => a + b;
+
+module.exports = {
+	addTwoNumbers,
+} 
