91caebdfaec4929b56bf3bf735e9815b9775a6f6
Guren <Kristjan.Veensalu@gmail.com>
2019-10-07 02:48:14 +0300

    MongoDB päringud

--- server/server.js
+++ server/server.js
@@ -1,60 +1,20 @@
 require("dotenv").config({path:"C:/Users/GurenZify/Rprogrammeerimine/.env" });
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const path= require("path");
 const PORT = process.env.PORT||5000;
 const DB = require("./database.js");
 const mongoose = require("mongoose");
+const userRouter = require("./user.js");
 
 
-console.log(process.env.DB_USERNAME);
 
-var kittySchema = new mongoose.Schema({
-	name:String
-});
-var Kitten = mongoose.model("Kitten", kittySchema);
-
-const kitten1 = new Kitten({
-	name: "Blue cat 3"
-});
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@rprogrammeerimine-gdys3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
+app.use(userRouter);
 
-
-mongoose.connect(DB_URL)
-	.then(() => {
-		console.log("DB access success!")
-		kitten1.save(err =>{
-			if(err){
-				console.log("Error");
-			}
-			else{
-				console.log("Success! Saved");
-			}
-		});
-	})
-	.catch( err =>{
-		console.error("error happened", err)
-	});
-
-
-/** Lisasime GET all items*/
-
-app.get("/api/items", (req, res)=>{
-	res.json(DB.getItems())
-});
-
-
-/**Lisasime GET item with ID*/
-app.get("/api/items/:itemId", (req,res) => {
-	res.send(DB.getItem(req.params.itemId));
-});
-
-app.post("/hello",(req, res)=>{
-	res.send("post hello!");
-});
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
@@ -66,9 +26,18 @@ app.get('/items/*', (req, res) => {
 
 app.use(express.static('dist'));
 
+function listen(){
+	app.listen(PORT, () => {
+		console.log(`Example app listening on port ${process.env.PORT||PORT}!`);
+		console.log(`http:localhost:${PORT}/index.html`);
+	});
+};
 
-app.listen(PORT, () => {
-	console.log(`Example app listening on port ${process.env.PORT||PORT}!`);
-	console.log(`http:localhost:${PORT}/index.html`);
+mongoose.connect(DB_URL)
+	.then(() => {
+		console.log("DB access success!")
+		listen();
+		})
+	.catch( err =>{
+		console.error("error happened", err)
 });
-
