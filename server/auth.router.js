f1ecccd23734c402234f7ac41cac9d5e2686a38e
Guren <Kristjan.Veensalu@gmail.com>
2019-12-07 16:39:49 +0200

    lesson10/11

--- server/auth.router.js
+++ server/auth.router.js
@@ -1,47 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const { check, validationResult } = require("express-validator");
 const userController = require("./user.controller.js");
 const jwt = require("jsonwebtoken");
 
 const validationMiddleware = (req, res, next) => {
-	const errors = validationResult(req);
-  	if (!errors.isEmpty()) {
-   		return res.status(422).json({ errors: errors.array() });
-  }
-  next();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errors: errors.array()});
+    }
+    next();
 };
 
-router.post("/verify", (req, res) =>{
-	const bearerHeader = req.headers["authorization"];
-	if(!bearerHeader) return res.send(400);
-	const token = bearerHeader.split(" ")[1];
-	if(!token) return res.send(400);
-	jwt.verify( token, process.env.JWT_KEY, (err, decoded) => {
-    	if(err) {
-    		return res.status(401).send(err);
-    	}
-    	res.status(200).send(decoded);
-	});
+router.post("/verify", (req, res) => {
+    const bearerHeader = req.headers["authorization"];
+    if(!bearerHeader) return res.send(400);
+    const token = bearerHeader.split(" ")[1];
+    if(!token) return res.send(400);
+    jwt.verify( token, process.env.JWT_KEY, (err, decoded) => {
+        if(err) {
+            console.log(err);
+            return res.status(401).send(err);
+        }
+        res.status(200).send(decoded);
+    });
 });
 
 router.post("/login", userController.login);
 
-
-router.post("/signup", [
-		check("email").isEmail().normalizeEmail(),
-		check("password")
-		.isLength({ min: 5 })
-		.withMessage("must be at least 5 chars long")
-		.matches(/\d/).withMessage("must contain a number")
-		.not().isIn(["123", "password1", "parool1", "parool123"]).withMessage('Do not use a common word as the password')
-	],
-	validationMiddleware,
-	userController.signup
+router.post("/signup", 
+[
+    check("email").isEmail().normalizeEmail(),
+    check("password").isLength( {min:5}).withMessage("Must be atleast 5 chars long")
+],
+validationMiddleware,
+userController.signup
 );
-
-
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,97 +1,80 @@
-//const result = require("dotenv").config();
 const express = require('express');
 const app = express();
-const path= require("path");
-const PORT = process.env.PORT||3000;
-const DB = require("./database.js");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-const itemRouter = require("./item.router.js");
-const userRouter = require("./user.router.js");
-const authRouter = require("./auth.router.js");
-
+const DB = require("./database.js");
 const Item = require("./item.model.js");
-const bodyParser = require("body-parser");
-/** Development environment. In Heroku we don't use .env file */
-let result = 0
-if(process.env.NODE_ENV !== "production"){
- result = require('dotenv').config();
+const bodyParser= require("body-parser");
+const apiRouter = require("./apiRouter.js");
 
+/** Development environment. In Heroku we don't use .env file */
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config();
 }
 
-console.log(result);
-
-const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@rprogrammeerimine-gdys3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-qfjwa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 app.use(bodyParser.json());
-
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1", itemRouter);
-app.use("/api/v1/users", userRouter);
-
+app.use(apiRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
-})
+    res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+});
 
 app.get('/items/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
-})
+});
 
 app.use(express.static('dist'));
 
-function listen(){
-	app.listen(PORT, () => {
-		console.log(`Example app listening on port ${process.env.PORT||PORT}!`);
-		console.log(`http:localhost:${PORT}/index.html`);
-	});
-};
+function listen() {
+  app.listen(PORT, () => {
+    console.log("Server started", PORT);
+    console.log('http://localhost:${PORT}');
+  });
+}
 
 mongoose.connect(DB_URL)
-	.then(() => {
-		console.log("DB access success!")
-		migrate();
-		//deleteAllItems();
-		listen();
-		})
-	.catch( err =>{
-		console.error("error happened", err)
-});
+  .then(() => {
+    console.log("Database access success!");
+    listen();
+    migrate();
+    //deleteAllItems();
+  })
+  .catch( err => {
+    console.log("error happened", err);
+  });
 
+  function migrate(){
+      Item.count({},(err, countNr) => {
+          if(err) throw err;
+          if(countNr > 0 ){
+            console.log("Already had items, don't save");
+            return;
+          } 
+          saveAllItems();
+      });
+  }
 
+  function deleteAllItems(){
+      Item.deleteMany({}, (err, doc) => {
+        console.log('err', err, "doc", doc);
+      });
+  }
 
-
-
-//Ei tea millal k6ik on salvestatud.
- function migrate(){
- 	Item.count({}, (err, countNr) =>{
- 		if(err) throw err;
- 		if(countNr > 0) {
- 			console.log("Aleady had items, wont save.");
- 			return;
- 		}
- 		saveAllItems();
- 	});
-}
-
-function deleteAllItems(){
-	Item.deleteMany({}, (err, doc) =>{
-		console.log("err", err, "doc", doc);
-	});
-};
-
-
-function saveAllItems(){
-	console.log("Migrate started...");
- 	const items = DB.getItems();
- 	items.forEach(item =>{
- 		const documentS = new Item(item);
- 		documentS.save((err) =>{
- 			if(err){
- 				console.log(err);
- 				throw new Error("Something happened during save");
- 			}
- 			console.log("save Success!!!");
- 		})
- 	});
- 	console.log("items", items);
-}
+  function saveAllItems(){
+    console.log("migrate started!");
+    const items = DB.getItems();
+    items.forEach(item=> {
+        const document = new Item(item);
+        document.save( (err) => {
+          if(err){
+              console.log(err);
+              throw new Error ("Something happened during save");
+          }
+          console.log('save succes');
+        });
+    });
+    console.log("items", items);
+  }
